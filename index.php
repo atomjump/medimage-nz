8294f7d380b74b740dc35f758c715c7ffc190aba
AtomJump <peter@atomjump.com>
2016-08-01 14:09:02 +1200

    typo

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 
   <body>
-      This is a placeholder. Coming soon... see our <a href="http://medimage.atomjump.com>Current Site</a>
+      This is a placeholder. Coming soon... see our <a href="http://medimage.atomjump.com">Current Site</a>
   </body>
 
 </html>
