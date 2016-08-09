f44e818c4f94b5ecad8dda761cd42295cabe53b1
AtomJump <peter@atomjump.com>
2016-08-09 15:16:10 +1200

    Original feedback code

---
+++ original-feedback.php
@@ -0,0 +1,49 @@
+<html>	
+	 <head>
+  	    <meta charset="utf-8">
+		 <meta name="viewport" content="width=device-width, user-scalable=no">
+		 <title>MedImage App - phone camera to PC directly</title>
+		 <meta name="description" content="Photograph and catalogue your patient's health issues with your phone, and send them directly to your PC.">
+
+		 <meta name="keywords" content="TBC">
+
+			  <!-- Bootstrap core CSS -->
+			<link rel="StyleSheet" href="https://atomjump.com/css/bootstrap.min.css" rel="stylesheet">
+
+			<!-- AtomJump Feedback CSS -->
+			<link rel="StyleSheet" href="https://atomjump.com/css/comments-0.1.css">
+
+			<!-- Bootstrap HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+			<!--[if lt IE 9]>
+			  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+			  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+			<![endif]-->
+
+			<!-- Include your version of jQuery here.  This is version 1.9.1 which is tested with AtomJump Feedback. -->
+			<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+			<!-- Took from here 15 May 2014: http://ajax.googleapis.com/ajax/libs/jquery/1.9.1 -->
+
+			<!-- For the dropdown autocomplete -->
+			<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+			<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+
+
+			<script>
+				var ajFeedback = {
+					"uniqueFeedbackId" : "ajps_medimage",
+					"myMachineUser" : "111.69.60.34:8",
+					"server": "https://atomjump.com/api"				}
+			</script>
+			<script type="text/javascript" src="js/chat-1.0.js"></script>
+
+</head>
+
+
+<body>
+      <a class="comment-open" id="this-comment-open" href="javascript:" data-uniquefeedbackid="ajps_medimage" data-mymachineuser="111.69.60.34:8"  >
+			    <div id="logo-wrapper" class="looplogo">
+				      <img class="saver-hideable" src="images/medimage-www-logo.svg" id="bg" alt="">
+			    </div>
+			</a>
+<body>
+</html>
